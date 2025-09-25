a1dc00fb295b50dc0e572e65622ffa3707027f30
ayomidepeat <peatayomide@gmail.com>
2025-09-25 11:43:39 +0100

    feat:set up dio client

---
+++ lib/core/network/dio_client.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+
+class DioClient {
+  static Dio create() {
+    final dio = Dio(BaseOptions(
+      baseUrl: "http://127.0.0.1:8000",
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 15),
+      headers: {"Accept": "application/json"},
+    ));
+
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          
+          // options.headers["Authorization"] = "Bearer token";
+          return handler.next(options);
+        },
+        onResponse: (response, handler) {
+          return handler.next(response);
+        },
+        onError: (DioException e, handler) {
+          return handler.next(e);
+        },
+      ),
+    );
+    return dio;
+  }
+}
